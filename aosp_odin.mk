c96da28ff2fb1cbaab65599be9e86c3ebb359202
Alex-the-black <alex.mihalenok@gmail.com>
2016-12-28 12:23:08 +0300

    aospext fixes

--- aosp_odin.mk
+++ aosp_odin.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/sony/odin/full_odin.mk)
 
 # Inherit AOSP common Phone stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:5.1.1/10.7.A.0.222/4076033025:user/release-keys PRIVATE_BUILD_DESC="C6503-user 5.1.1 10.7.A.0.222 4076033025 test-keys"
 
